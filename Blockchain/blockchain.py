a19354807d1d459edb232291e3e2dd36d53c79a7
enby <enesbatin97@hotmail.com>
2021-12-08 00:47:59 +0300

    Proof of work function updated
    Function update and some  config file

--- Blockchain/blockchain.py
+++ Blockchain/blockchain.py
@@ -5,6 +5,7 @@ import json
 from flask import Flask, jsonify
 
 class Blockchain:
+    
     def __init__(self):
         self.chain = []
         self.create_block(proof = 1, previous_hash = '0')
@@ -17,4 +18,24 @@ class Blockchain:
                  }
         self.chain.append(block)
         return block
+    
+    def get_previous_block(self):
+        return self.chain[-1] #gives the last index of chain
+    
+    def proof_ow(self, previous_proof):
         
+        new_proof = 1
+        chck_proof = False
+        while chck_proof is False:
+            hash_operation = hashlib.sha256(str(new_proof**2 - previous_proof**2).encode()).hexdigest()
+            if hash_operation[:4] == '0000':
+                chck_proof = True
+            else:
+                new_proof += 1
+            return new_proof
+            
+        
+
+#Mining (etc.)
+
+
