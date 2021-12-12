aa511738b9d86325554df6456df3c8d39cbc2fd7
enby <enesbatin97@hotmail.com>
2021-12-12 18:14:08 +0300

    Functions updated

--- Blockchain/blockchain.py
+++ Blockchain/blockchain.py
@@ -57,4 +57,36 @@ class Blockchain:
 
 #Mining (etc.)
 
+app = Flask(__name__)
+
+blockchain = Blockchain()
+
+#mining new block
+@app.route('/mine_block', methods=['GET'])
+def mine_block():
+    previous_block = blockchain.get_previous_block()
+    previous_proof = previous_block['proof']
+    proof = blockchain.proof_ow(previous_proof)
+    previous_hash = blockchain.hash(previous_block)
+    block = blockchain.create_block(proof,previous_hash)
+    response = {'message' : 'congrats man, wen MOON !?',
+                'index' : block['index'],
+                'timestamp' : block['timestamp'],
+                'proof' : block['proof'],
+                'previous_hash' : block['previous_hash']
+                }
+    return jsonify(response), 200
+
+#get blockchain
+
+@app.route('/get_chain', methods=['GET'])
+def get_chain():
+    response = {'chain : ' : blockchain.chain,
+                'lenght ' : len(blockchain.chain)
+                }
+    return jsonify(response),200
+
+app.run(host=)
+
+
 
